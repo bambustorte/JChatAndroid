4cee6dd0f4a9700e8038fa17680dc91524b1380b
hama17zp <hama17zp@student.ju.se>
2018-01-09 01:21:03 +0100

    updated IAGT (IdeasAndGeneralTodos)

--- app/src/main/java/de/drunkenapps/jchat/IdeasAndGeneralTodos.java
+++ app/src/main/java/de/drunkenapps/jchat/IdeasAndGeneralTodos.java
@@ -8,4 +8,9 @@ package de.drunkenapps.jchat;
 abstract class IdeasAndGeneralTodos {
     private IdeasAndGeneralTodos(){}
     //Idea: when deleting users, deleting their groups, except there are other users in there
+
+    //Idea: create different user roles like moderators, admins etc.
+
+
+    //Todo: add every user to the public group chat
 }
